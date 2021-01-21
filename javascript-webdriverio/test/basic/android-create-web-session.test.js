1be76073ef9e4c4e5418a623c6b10fd19649bbf0
only4arunkumar <only4arunkumar@gmail.com>
2021-01-21 15:39:41 +1100

    Add files via upload
    initial commit

---
+++ javascript-webdriverio/test/basic/android-create-web-session.test.js
@@ -0,0 +1,27 @@
+const webdriverio = require('webdriverio');
+const androidOptions = require('../../helpers/caps').androidWebOptions;
+const assert = require('chai').assert;
+const expect = require('expect.js');
+describe('Load the player', ()=> {
+
+
+  it('should create and destroy Android browser session',  async function(){
+   
+    const client =  await webdriverio.remote(androidOptions);
+    await client.url('https://motivation.vudoo.io/embed/12373082862');
+    await client.pause(5000);
+    expect(((await client.$("button.vjs-big-play-button")).isDisplayed)).to.be.ok();
+    expect(((await client.$("button.vjs-big-play-button")).isEnabled)).to.be.ok();
+    expect(((await client.$("button.vjs-big-play-button")).isClickable)).to.be.ok();
+    client.elementClick(client.$("button.vjs-big-play-button"));
+   
+   
+  
+
+   client.deleteSession();
+
+
+    
+  });
+});
+ 
